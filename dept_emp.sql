20011fad07579db8ed2671a069265d18b478a690
Tasnia Wahid <137854789+tasniawahid7@users.noreply.github.com>
2023-12-21 03:06:21 -0500

    Delete dept_emp.sql

--- dept_emp.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE dept_emp (
-    emp_no INT,
-    dept_no VARCHAR(4),
-    FOREIGN KEY (dept_no) REFERENCES departments(dept_id),
-    FOREIGN KEY (emp_no) REFERENCES employees(emp_id)
-);
-COPY dept_emp FROM 'C:\Users\Tasnia Wahid\Downloads\Starter_Code (5)\Starter_Code\data\dept_emp.csv' DELIMITER ',' CSV HEADER;
