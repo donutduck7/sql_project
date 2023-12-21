89b0d14a8ec135cc123be18115002bdfd8014968
Tasnia Wahid <137854789+tasniawahid7@users.noreply.github.com>
2023-12-21 03:07:06 -0500

    Delete salaries.sql

--- salaries.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE salaries (
-    emp_no INT PRIMARY KEY,
-    salary INT
-);
-COPY salaries FROM 'C:\Users\Tasnia Wahid\Downloads\Starter_Code (5)\Starter_Code\data\salaries.csv' DELIMITER ',' CSV HEADER;
