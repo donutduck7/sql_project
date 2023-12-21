966d27c1242e0f0e1ea28cb56526036042cfb886
Tasnia Wahid <137854789+tasniawahid7@users.noreply.github.com>
2023-12-21 03:06:27 -0500

    Delete dept_manager.sql

--- dept_manager.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE Dept_Manager (
-    emp_no INT,
-    dept_no VARCHAR(4),
-    FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
-    FOREIGN KEY (dept_no) REFERENCES Departments(dept_id),
-    PRIMARY KEY (dept_no, emp_no)
-    -- Other constraints as necessary
-);
-COPY dept_manager FROM 'C:\Users\Tasnia Wahid\Downloads\Starter_Code (5)\Starter_Code\data\dept_manager.csv' DELIMITER ',' CSV HEADER;
